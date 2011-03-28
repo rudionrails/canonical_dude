3d8e303fe2b76ab07dfe8bb20644760ab13dcef0
Rudolf Schmidt <hello@rudionrails.com>
2011-03-28 14:13:37 +0200

    refactored url generation into helper methods

--- lib/canonical_dude/controller_methods.rb
+++ lib/canonical_dude/controller_methods.rb
@@ -1,24 +1,7 @@
 module CanonicalDude::ControllerMethods
 
   def canonical_url( url_for_options = {} )
-    url = case
-    when Hash then url_for( url_for_options )
-    when String then url_for_options
-    else # could be an AR instance, so let's try some custom methods
-      # will turn a User instance into 'user'
-      canonical_method_name = url_for_options.class.name.downcase.gsub( '::', '_' )
-
-      custom_canonical_method_name = [
-        "canonical_#{canonical_method_name}_url",
-        "canonical_#{canonical_method_name}_path",
-        "canonical_url_for"
-      ].find { |m| respond_to? m }
-
-      return url_for( url_for_options ) unless custom_canonical_method_name
-      send( custom_canonical_method_name, url_for_options )
-    end
-
-    self.instance_variable_set( "@_canonical_url", url )
+    self.instance_variable_set( "@_canonical_url_for_options", url_for_options )
   end
 
 end
